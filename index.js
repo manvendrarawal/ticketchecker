298bdeb215fdb766d3fad91dd3868823c94e43bf
Manvendra <msrawal2001@gmail.com>
2024-12-26 14:36:44 +0530

    Middleware added for ride1 and ride2

--- index.js
+++ index.js
@@ -13,9 +13,44 @@ function ticketChecker(req,res,next){
     }
 }
 
-app.use(ticketChecker);
+function isOldEnough(age){
+    if(age>14){
+        return true;
+    }
+    else{
+        false;
+    }
+}
 
 
-app.get("/ride1",function(){
-    
-})
+//Middle ware for the Old enough 
+
+function isOldEnoughMiddleware(req,res,next){
+    const age = req.query.age;
+    console.log(age);
+    if(age >=14){
+        next();
+    }
+    else{
+        res.json({
+            msg : "You are not of enough age"
+        })
+    }
+}
+
+app.get("/ride1",isOldEnoughMiddleware , function(req,res){
+    res.json({
+        msg : "You have successfully riden the ride 1"
+    })
+});
+
+
+
+app.get("/ride2",isOldEnoughMiddleware,function(req,res){
+ res.json({
+    msg : "You have successfully ride the ride 2"
+ });
+});
+
+
+app.listen(3000);
